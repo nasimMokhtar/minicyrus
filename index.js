cead3a24ac75574f70371ac364b80fd4ec507466
Nasim Mokhtar <nasim.mokhtar83@gmail.com>
2020-12-07 16:17:16 +0100

    ini commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const winston = require('winston');
+const express = require('express');
+
+const app = express();
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+
+const port = process.env.PORT || 4000;
+const server = app.listen(port, ()=> winston.info(`Listening to port ${port} ..`));
+module.exports = server;
